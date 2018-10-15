0d379ecaaddf8f0419fa5c3771c35fca5eebacd0
albertcoder <eralbert9191@gmail.com>
2018-10-15 12:56:31 -0700

    Added proper credits to the README.md file

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Dodge-the-enemy
- A classic arcade game
+# Clasic Arcade Game Project | Udacity FEND Nanodegree | 2018
+Project 3 for Udacity's Front-End Web Developer Nanodegree program; completed as part of the Grow with Google Scholarship Program, 2018.
+
+A classic arcade game based on Frogger that required students to use object-oriented JavaSCript to implement functionality to the provided art assets and game engine.
+
+## Authors
+
+Albert added to and edited code and assets provided by Udacity.
+
+## Getting Started
+
+1. Clone the repository to create a copy on your local machine. Click [here](https://help.github.com/articles/cloning-a-repository/ "GitHub Help — Clone a Repository") for help with this task from GitHub.
+
+2. Open the `index.html` file in your browser. Some tips for doing this from Help Desk Geek are available [here](https://helpdeskgeek.com/how-to/open-an-html-file-in-google-chrome/ ).
+
+    * As an alternative to steps one and two, you can run the repository's `index.html` file on [HTML Preview](http://htmlpreview.github.io/?https://albertcoder.github.io/Dodge-the-enemy/).
+
+3. Start playing!
+
+## Playing the Game
+
+* Your mission: get your avatar "home" to the destination on the other side of the canvas without colliding with any "enemies" — in this case: bugs.
+* To control player: use arrow keys — up, down, right and left.
+* If you collide with an enemy, your player will move back to the start position.
+* After your player makes it "home" safely, you will have the option of selecting to play again.
+
+## Acknowledgements
+
+* Udacity.com supplied the initial HTML file, art assets, game engine and image loading utility for this project.
+
+   * The author implemented the game's functionality by editing the app.js file and, where necessary, the engine.js file, utilizing object-oriented JavaScript wiht ES6 class syntax. The author also added headings and a popover to the HTML file, and styled both with CSS, and implemented popover functionality.
+
+* Portions of Udacity student Matthew Cranford's series of "Arcade Game Walkthrough" posts on the project were consulted: https://matthewcranford.com/. Udacity student Jacob Olson also helped in person implementing the game functionality including the cancelAnimation function during the meetups.
+
+* The popover markup, stylings and implementation were adapted from code found in w3schools' post "How TO - CSS/JS Modal": https://www.w3schools.com/howto/howto_css_modals.asp.
+
+* This README was based on a template provided by Billie Thompson posting under the name "PurpleBooth" at https://gist.github.com/PurpleBooth/109311bb0361f32d87a2. A post by Matias Singers posting under the username "matiassingers" at https://github.com/matiassingers/awesome-readme was also consulted.
+
+* Udacity's FEND Nanodegree program provided the structure and much of the information I needed to learn the basics of HTML, CSS and JavaScript -- including object-oriented JS -- which allowed me to successfully complete this project. My fellow Udacity students as well as the wider programming community also provided indispensable information, resources, support and encouragement.
+
+* Funding for my learning through Udacity including this project comes courtesy of the Grow with Google Scholarship program.
---
+++ js/app.js
@@ -0,0 +1,163 @@
+// Variable to decide if the game is over or not
+let levelComplete = false;
+
+// Enemies our player be aware of
+//class Enemy to create objects of class Enemy.
+class Enemy {
+    constructor(x, y) {
+        this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.y = y;
+    }
+
+    update(dt) {
+        this.x = this.x + 65 * dt;
+        if (this.x > 500) {
+            this.x = 0;
+        }
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+// Class to update the enemy's position at a slower pace
+class EnemySlow extends Enemy {
+    update(dt) {
+        this.x = this.x + 20 * dt;
+        if (this.x > 500) {
+            this.x = 0;
+        }
+    }
+}
+
+// Class to update the enemy's position at a fast pace
+class EnemyFast extends Enemy {
+    update(dt) {
+        this.x = this.x + 100 * dt;
+        if (this.x > 520) {
+            this.x = 0;
+        }
+    }
+}
+
+//Class player that attempts to cross the area and reach the river
+class Player {
+    constructor() {
+        this.sprite = 'images/char-cat-girl.png';
+        this.x = 200;
+        this.y = 385;
+    }
+
+    update(dt) {}
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    // Function to handle the inputs of the user
+    handleInput(k) {
+        switch (k) {
+            case 'left':
+                this.x = this.x - 20;
+                if (this.x < -45) {
+                    this.x = -45;
+                };
+                break;
+            case 'right':
+                this.x += 20;
+                if (this.x > 445) {
+                    this.x = 445;
+                };
+                break;
+            case 'up':
+                this.y -= 20;
+                break;
+            case 'down':
+                this.y += 20;
+                if (this.y > 415) {
+                    this.y = 415;
+                };
+                break;
+        }
+    }
+// Function which checks for the collision 
+    checkForCollision() {
+        let xLow = this.x - 48;
+        let xHigh = this.x + 48;
+        let yLow = this.y - 40;
+        let yHigh = this.y + 40;
+        allEnemies.forEach(function (e) {
+            //Checks for the condition if the player
+            //falls into the collision window
+            //reset the position to the original
+            if (e.x > xLow && e.x < xHigh && e.y > yLow && e.y < yHigh) {
+                player.x = 200;
+                player.y = 405;
+            };
+        });
+    }
+
+    checkForFinish() {
+        if (this.y < -10) {
+            document.body.querySelector('#completion-popover').style.display = 'block';
+            levelComplete = true;
+        }
+    }
+}
+// Instantiation of the objects to create game entities
+const enemy1 = new Enemy(25, 220);
+const enemy2 = new Enemy(200, 220);
+const enemy3 = new EnemySlow(-140, 130);
+const enemy4 = new EnemySlow(10, 130);
+const enemy5 = new EnemySlow(210, 130);
+const enemy6 = new EnemyFast(300, 50);
+const enemy7 = new EnemyFast(5, 50);
+const enemy8 = new EnemyFast(-100, 50);
+
+// All the enemy-objects are put into an array
+let allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7, enemy8];
+let player = new Player();
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function (e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+/*
+ * Functionality for popover adapted from the w3schools tutorial
+ * "How TO - CSS/JS Modal": https://www.w3schools.com/howto/howto_css_modals.asp.
+ */
+const popoverClose = document.body.querySelectorAll('.popover-close');
+const popover = document.body.querySelectorAll('.popover');
+const completionPopoverClose = document.body.querySelector('#completion-popover-close');
+
+// Designed to handle possiblity of multiple popovers on page.
+popoverClose.forEach(function(x) {
+  x.addEventListener('click', closePopover);
+});
+
+function closePopover() {
+  popover.forEach(function(x) {
+    x.style.display = 'none';
+  });
+  // Reload page if completion popover is displayed.
+  if (this.id ='completion-popover-close') {
+    location.reload();
+  };
+}
+
+// Refresh page if user selects to play again on displaying completion popover.
+document.querySelector("#play-again-button").addEventListener('click', function () {
+  location.reload();
+});
